275ed52580d17061df328d87fc7423fe2205ed06
Kania <kania@ai.local>
2026-04-09 06:56:18 +0000

    feat: Add complete ABIs for contracts

--- app/wagmi.ts
+++ app/wagmi.ts
@@ -18,16 +18,50 @@ export const CONTRACT_ADDRESSES = {
   marketplace: '0xF22d0FB68f542E681344B0130Bf284c3Fe2ddc7E',
 };
 
-// Simple ABIs
+// Complete ABI - MyNFT (simplified - main functions)
 export const NFT_ABI = [
-  'function mint(address to, uint256 quantity)',
-  'function balanceOf(address account) view returns (uint256)',
-  'function ownerOf(uint256 tokenId) view returns (address)',
-];
+  { "inputs": [{"internalType":"string","name":"_name","type":"string"},{"internalType":"string","name":"_symbol","type":"string"},{"internalType":"string","name":"_baseURI","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},
+  { "anonymous": false, "inputs": [{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"approved","type":"address"},{"indexed":true,"name":"tokenId","type":"uint256"}],"name": "Approval", "type": "event" },
+  { "anonymous": false, "inputs": [{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"operator","type":"address"},{"indexed":false,"name":"approved","type":"bool"}],"name": "ApprovalForAll", "type": "event" },
+  { "anonymous": false, "inputs": [{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":true,"name":"tokenId","type":"uint256"}],"name": "Transfer", "type": "event" },
+  { "inputs": [{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name": "approve", "outputs": [], "stateMutability":"payable","type":"function" },
+  { "inputs": [{"internalType":"address","name":"owner","type":"address"}],"name": "balanceOf", "outputs": [{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function" },
+  { "inputs": [], "name": "baseURI", "outputs": [{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function" },
+  { "inputs": [{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256[]","name":"tokenIds","type":"uint256[]"}],"name": "batchMint", "outputs": [], "stateMutability":"nonpayable","type":"function" },
+  { "inputs": [{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name": "getApproved", "outputs": [{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function" },
+  { "inputs": [{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name": "isApprovedForAll", "outputs": [{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function" },
+  { "inputs": [], "name": "maxSupply", "outputs": [{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function" },
+  { "inputs": [{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"quantity","type":"uint256"}],"name": "mint", "outputs": [], "stateMutability":"nonpayable","type":"function" },
+  { "inputs": [], "name": "name", "outputs": [{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function" },
+  { "inputs": [], "name": "owner", "outputs": [{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function" },
+  { "inputs": [{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name": "ownerOf", "outputs": [{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function" },
+  { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability":"nonpayable","type":"function" },
+  { "inputs": [{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name": "safeTransferFrom", "outputs": [], "stateMutability":"payable","type":"function" },
+  { "inputs": [{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name": "safeTransferFrom", "outputs": [], "stateMutability":"payable","type":"function" },
+  { "inputs": [{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name": "setApprovalForAll", "outputs": [], "stateMutability":"nonpayable","type":"function" },
+  { "inputs": [{"internalType":"string","name":"_baseURI","type":"string"}],"name": "setBaseURI", "outputs": [], "stateMutability":"nonpayable","type":"function" },
+  { "inputs": [{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name": "supportsInterface", "outputs": [{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function" },
+  { "inputs": [], "name": "symbol", "outputs": [{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function" },
+  { "inputs": [{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name": "tokenURI", "outputs": [{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function" },
+  { "inputs": [], "name": "totalMinted", "outputs": [{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function" },
+  { "inputs": [], "name": "totalSupply", "outputs": [{"internalType":"uint256","name":"result","type":"uint256"}],"stateMutability":"view","type":"function" },
+  { "inputs": [{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name": "transferFrom", "outputs": [], "stateMutability":"payable","type":"function" },
+  { "inputs": [{"internalType":"address","name":"newOwner","type":"address"}],"name": "transferOwnership", "outputs": [], "stateMutability":"nonpayable","type":"function" },
+] as const;
 
+// Complete ABI - NFTMarketplace
 export const MARKETPLACE_ABI = [
-  'function createListing(address nftContract, uint256 tokenId, uint256 price)',
-  'function buyListing(uint256 listingId) payable',
-  'function cancelListing(uint256 listingId)',
-  'function getListing(uint256 listingId) view returns (address, address, uint256, uint256, bool)',
-];
+  { "anonymous": false, "inputs": [{"indexed":true,"name":"listingId","type":"uint256"}],"name": "ListingCancelled", "type": "event" },
+  { "anonymous": false, "inputs": [{"indexed":true,"name":"listingId","type":"uint256"},{"indexed":true,"name":"seller","type":"address"},{"indexed":false,"name":"nftContract","type":"address"},{"indexed":false,"name":"tokenId","type":"uint256"},{"indexed":false,"name":"price","type":"uint256"}],"name": "ListingCreated", "type": "event" },
+  { "anonymous": false, "inputs": [{"indexed":true,"name":"listingId","type":"uint256"},{"indexed":true,"name":"buyer","type":"address"},{"indexed":false,"name":"price","type":"uint256"}],"name": "NFTBought", "type": "event" },
+  { "inputs": [{"internalType":"uint256","name":"listingId","type":"uint256"}],"name": "buyListing", "outputs": [], "stateMutability":"payable","type":"function" },
+  { "inputs": [{"internalType":"uint256","name":"listingId","type":"uint256"}],"name": "cancelListing", "outputs": [], "stateMutability":"nonpayable","type":"function" },
+  { "inputs": [{"internalType":"address","name":"nftContract","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"uint256","name":"price","type":"uint256"}],"name": "createListing", "outputs": [], "stateMutability":"nonpayable","type":"function" },
+  { "inputs": [], "name": "getActiveListings", "outputs": [{"components":[{"internalType":"address","name":"seller","type":"address"},{"internalType":"address","name":"nftContract","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"uint256","name":"price","type":"uint256"},{"internalType":"bool","name":"active","type":"bool"}],"internalType":"struct NFTMarketplace.Listing[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function" },
+  { "inputs": [{"internalType":"uint256","name":"listingId","type":"uint256"}],"name": "getListing", "outputs": [{"components":[{"internalType":"address","name":"seller","type":"address"},{"internalType":"address","name":"nftContract","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"uint256","name":"price","type":"uint256"},{"internalType":"bool","name":"active","type":"bool"}],"internalType":"struct NFTMarketplace.Listing","name":"","type":"tuple"}],"stateMutability":"view","type":"function" },
+  { "inputs": [{"internalType":"address","name":"user","type":"address"}],"name": "getUserListings", "outputs": [{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function" },
+  { "inputs": [], "name": "listingIdCounter", "outputs": [{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function" },
+  { "inputs": [{"internalType":"uint256","name":"","type":"uint256"}],"name": "listings", "outputs": [{"internalType":"address","name":"seller","type":"address"},{"internalType":"address","name":"nftContract","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"uint256","name":"price","type":"uint256"},{"internalType":"bool","name":"active","type":"bool"}],"stateMutability":"view","type":"function" },
+  { "inputs": [], "name": "platformFee", "outputs": [{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function" },
+  { "inputs": [{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name": "userListings", "outputs": [{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function" },
+] as const;
